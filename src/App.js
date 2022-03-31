f31260cf56bbf4a17d135903cb09906b5a1f3139
Leonardo Gonçalves <souoleo@gmail.com>
2022-03-30 23:55:36 -0300

    test useEffect on document.title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useEffect, useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = "Set name of counts " + count;
+  });
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Clicks {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
